c6d7b6b35addf88144234540f120421a298f8afc
Timothy Pendarvis <trpendarvis@gmail.com>
2022-08-30 16:29:57 -0600

    this commit will pass all test in mythical-UNICORN

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,9 +1,16 @@
 class Unicorn {
-  constructor(name, color) {
+  constructor(name, color = "white", isWhite) {
     this.name = name;
     this.color = color;
+    this.isWhite = function () {
+      if (this.color != "white") {
+        return false;
+      }
+    };
+    this.says = function (say) {
+      return `**;* ${say} *;**`
+    };
   }
 }
-
 
 module.exports = Unicorn;
